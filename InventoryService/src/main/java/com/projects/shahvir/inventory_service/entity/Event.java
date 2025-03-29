1c073318d191ffbd3286123e6e8548f4b3d0b3f6
Shahvir <shahvir05@icloud.com>
2025-03-29 11:34:03 -0400

    Multi-Module Project Structure v2

---
+++ InventoryService/src/main/java/com/projects/shahvir/inventory_service/entity/Event.java
@@ -0,0 +1,32 @@
+package com.projects.shahvir.inventory_service.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "event")
+public class Event {
+
+    @Id
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "total_capacity")
+    private Long totalCapacity;
+
+    @Column(name = "left_capacity")
+    private Long leftCapacity;
+
+    @ManyToOne
+    @JoinColumn(name = "venue_id")
+    private Venue venue;
+}
---
+++ src/main/java/com/shahvir/EventBookingApp/EventBookingAppApplication.java
@@ -0,0 +1,13 @@
+package com.shahvir.EventBookingApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventBookingAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventBookingAppApplication.class, args);
+	}
+
+}
