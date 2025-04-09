f9332a710af5113ce6a95f21a377d66f0eda0c4f
Shahvir <shahvir05@icloud.com>
2025-04-09 15:42:57 -0400

    added Keycloak integration for oath2 JWT authentication

---
+++ APIGateway/src/main/java/com/shahvir/api_gateway/config/SecurityConfig.java
@@ -0,0 +1,32 @@
+package com.shahvir.api_gateway.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+import javax.print.attribute.standard.NumberUpSupported;
+
+@Configuration
+public class SecurityConfig {
+
+    @Value("${keycloak.auth.jwk-set-uri}")
+    private String jwkSetUri;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity.authorizeHttpRequests(authorizeRequests ->
+                authorizeRequests.anyRequest().authenticated())
+                .oauth2ResourceServer(oath -> oath.jwt(Customizer.withDefaults()))
+                .build();
+    }
+
+    @Bean
+    public JwtDecoder jwtDecoder() {
+        return NimbusJwtDecoder.withJwkSetUri(jwkSetUri).build();
+    }
+}
