f30bf479bcf9239d79dcf9ddd69016dcc7f562c0
Shahvir <shahvir05@icloud.com>
2025-04-08 23:03:59 -0400

    initialized order service

---
+++ OrderService/src/main/java/com/projects/shahvir/order_service/service/OrderService.java
@@ -0,0 +1,32 @@
+package com.projects.shahvir.order_service.service;
+
+
+import com.projects.shahvir.order_service.entity.Order;
+import com.shahvir.booking_service.event.BookingEvent;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+public class OrderService {
+
+    @KafkaListener(topics = "booking", groupId = "order-service")
+    public void orderEvent(BookingEvent bookingEvent) {
+        log.info("Received order event: {}", bookingEvent);
+
+        //Create Order object for DB
+        Order order = createOrder(bookingEvent);
+
+        //Update Inventory
+    }
+
+    private Order createOrder(BookingEvent bookingEvent) {
+        return Order.builder()
+                .customerId(bookingEvent.getUserId())
+                .eventId(bookingEvent.getEventId())
+                .ticketCount(bookingEvent.getTicketCount())
+                .totalPrice(bookingEvent.getTotalPrice())
+                .build();
+    }
+}
